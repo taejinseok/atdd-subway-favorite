16edb690d3771aa272819eff7b3f4bc1e915b95c
taejinseok <stj930@gmail.com>
2020-07-23 21:13:03 +0900

    style : format

--- src/main/java/wooteco/subway/SubwayAdminApplication.java
+++ src/main/java/wooteco/subway/SubwayAdminApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SubwayAdminApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SubwayAdminApplication.class, args);
     }
